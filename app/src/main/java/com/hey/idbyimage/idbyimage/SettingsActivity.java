0e8cfad2c29e220b665dc11fb94f058f316e12d9
teliran <teliran@gmail.com>
2019-01-13 20:32:54 +0200

    remove dep

--- app/src/main/java/com/hey/idbyimage/idbyimage/SettingsActivity.java
+++ app/src/main/java/com/hey/idbyimage/idbyimage/SettingsActivity.java
@@ -1,16 +1,11 @@
 package com.hey.idbyimage.idbyimage;
 
-import android.app.Activity;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.content.SharedPreferences;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceFragmentCompat;
-import android.support.v7.preference.PreferenceScreen;
 import android.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
