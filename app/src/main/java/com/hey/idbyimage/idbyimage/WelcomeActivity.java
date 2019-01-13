c933c5760f6874c4a508204a671bb45fb2489323
Yair Landmann <landmann@gmail.com>
2019-01-13 10:50:19 +0200

    Change the flow of the app -> from welcome if does rating go to menu, (need to consider if we allow users to edit there ratings and then just call the first SetUp again

--- app/src/main/java/com/hey/idbyimage/idbyimage/WelcomeActivity.java
+++ app/src/main/java/com/hey/idbyimage/idbyimage/WelcomeActivity.java
@@ -1,10 +1,17 @@
 package com.hey.idbyimage.idbyimage;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
     private Button next;
@@ -19,9 +26,30 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         Intent intent = null;
-        if(v.getId()==R.id.nextbtn)
-            intent=new Intent(this, InstructionActivity.class);
+        if(v.getId()==R.id.nextbtn) {
+            SharedPreferences pref = getSharedPreferences("imagePref", Context.MODE_PRIVATE);
+            if (pref.getAll().size() == CountImages())
+                intent = new Intent(this, MenuActivity.class);
+            else
+                intent = new Intent(this, InstructionActivity.class);
+        }
         if(intent!=null)
             startActivity(intent);
+    }
+
+    private int CountImages() {
+        Field[] fields = R.drawable.class.getFields();
+        List<Integer> drawables = new ArrayList<Integer>();
+        for (Field field : fields) {
+            // Take only those with name starting with "p"
+            if (field.getName().startsWith("p")) {
+                try {
+                    drawables.add(field.getInt(null));
+                } catch (IllegalAccessException e) {
+                    Toast.makeText(this,"Problem counting images",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        return drawables.size();
     }
 }
