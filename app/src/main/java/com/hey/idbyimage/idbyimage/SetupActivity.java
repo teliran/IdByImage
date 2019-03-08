00b1c9415286c9d112dced5ac6ab1422deba2cb5
henros0 <henros0@gmail.com>
2019-03-08 16:55:08 +0200

    Setup activity now supports the help button '?' clicking it prompts a dialog - which we still have to choose some instructions for.

--- app/src/main/java/com/hey/idbyimage/idbyimage/SetupActivity.java
+++ app/src/main/java/com/hey/idbyimage/idbyimage/SetupActivity.java
@@ -1,8 +1,10 @@
 package com.hey.idbyimage.idbyimage;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +21,7 @@ import java.util.List;
 public class SetupActivity extends AppCompatActivity implements View.OnClickListener {
     SharedPreferences imagePref;
 
-    private Button back,next;
+    private Button back,next, helpBtn;
     private SeekBar rating1,rating2;
     private ImageView image1,image2;
     private TextView indicator,rating1prog,rating2prog;
@@ -50,6 +52,7 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
     private void setButtonListener() {
         back.setOnClickListener(this);
         next.setOnClickListener(this);
+        helpBtn.setOnClickListener(this);
     }
 
     private int CountImages() {
@@ -116,6 +119,7 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
         numOfPage=1;
         back = findViewById(R.id.backbtn);
         next = findViewById(R.id.nextbtn);
+        helpBtn = findViewById(R.id.helpBtn);
         rating1=findViewById(R.id.rating1);
         rating2=findViewById(R.id.rating2);
         image1=findViewById(R.id.imagePlace1);
@@ -132,6 +136,8 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
             handleBackClick();
         else if(v==next)
             handleNextClick();
+        else if (v==helpBtn)
+            popDialog();
     }
 
     private void handleBackClick() {
@@ -210,6 +216,20 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
             numOfPage++;
             loadSetupScreen();
         }
+    }
+
+    private void popDialog(){
+        final AlertDialog.Builder ad = new AlertDialog.Builder(this);
+        ad.setTitle("Instructions");
+        ad.setMessage("These are some instructions");
+        ad.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        //ad.create();
+        ad.show();
     }
 
 }
