7f378d35a5520153de6e729d669b5d0e226eab42
henros0 <henros0@gmail.com>
2019-04-13 14:48:44 +0300

    Data Collector + Client + Tests users, ratings, healthcheck.

--- app/src/main/java/com/hey/idbyimage/idbyimage/Utils/DataCollector.java
+++ app/src/main/java/com/hey/idbyimage/idbyimage/Utils/DataCollector.java
@@ -1,16 +1,19 @@
 package com.hey.idbyimage.idbyimage.Utils;
 
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 
+import java.io.DataOutputStream;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
 /**
  * DataCollector - Single Object intended to collect the user's data,
@@ -19,13 +22,15 @@ import java.net.URL;
 public class DataCollector {
 
     private static  final String SERVER_URL = "http://132.72.23.63:3001";
-    //private static final String PORT = "3001";
     private static DataCollector dataCollector = new DataCollector();
     private boolean serverStatus;
+    private HashMap<String, Integer> allImagesRatings;
+
 
 
     private DataCollector(){
         this.serverStatus = checkServerStatus();
+        this.allImagesRatings = new HashMap<>();
     }
 
     public static DataCollector getDataCollectorInstance(){
@@ -33,7 +38,7 @@ public class DataCollector {
     }
 
     private boolean checkServerStatus(){
-        //Create a URL object holding our url
+        //Create a URL object holding our url : SERVER_URL+"/healthcheck"
         try {
             URL myUrl = new URL(SERVER_URL+"/healthcheck");
             //Create a connection
@@ -46,9 +51,12 @@ public class DataCollector {
 
             //Connect to our url
             connection.connect();
-            return true;
+            if(connection.getResponseCode() == 200) {
+                return true;
+            }
+            return false;
         }catch(Exception e){
-            String s = e.getMessage();
+            //String s = e.getMessage();
             return false;
         }
     }
@@ -57,11 +65,108 @@ public class DataCollector {
         return this.serverStatus;
     }
 
+    public void setImageRatingsData(HashMap<String, Integer> map){
+        this.allImagesRatings = map;
+    }
+
+    public boolean sendAllUserRatingsToServer(String userID){
+        if (!serverStatus){
+            return false;
+        }
+        for (Map.Entry<String, Integer> imageRatingEntry: allImagesRatings.entrySet()){
+            JSONObject userImageRating = getImageRatingJsonObject(userID, imageRatingEntry.getKey(), imageRatingEntry.getValue());
+            if(!sendDataToServer(userImageRating, "/ratings")){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean sendUserDataToServer(String userID, String age, String gender){
+        if (!serverStatus){
+            return false;
+        }
+        JSONObject userJson = getUserJsonObject(userID, age, gender);
+        if (!sendDataToServer(userJson, "/users")){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean sendDataToServer(JSONObject jsonObj, String urlPrefix){
+        if (!serverStatus){
+            return false;
+        }
+        String url = SERVER_URL+urlPrefix;
+        try {
+            URL myUrl = new URL(url);
+            //DataInputStream input;
+            HttpURLConnection connection = (HttpURLConnection)
+                    myUrl.openConnection();
+            connection.setConnectTimeout(15000);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setRequestProperty("Content-Type","application/json;charset=UTF-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.connect();
+            Log.i("JSON", jsonObj.toString());
+            DataOutputStream printout = new DataOutputStream((connection.getOutputStream()));
+            printout.writeBytes(jsonObj.toString());
+            printout.flush();
+            printout.close();
+            Log.i("STATUS", String.valueOf(connection.getResponseCode()));
+            Log.i("MSG" , connection.getResponseMessage());
+
+            connection.disconnect();
+            int resp = connection.getResponseCode();
+            if (connection.getResponseCode() == 200){
+                return true;
+            }else{
+                return false;
+            }
+
+        }catch (Exception e){
+            Log.e("ERROR send data:", e.getMessage());
+            return false;
+        }
+    }
 
 
+    @NonNull
+    private JSONObject getImageRatingJsonObject(String userID, String imageID, int rating) {
+        JSONObject userImageRating = new JSONObject();
+        try {
+            userImageRating.put("userId", userID);
+            userImageRating.put("imageId", imageID);
+            userImageRating.put("rating", rating + "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return userImageRating;
+    }
 
+    private JSONObject getUserJsonObject(String userID, String age, String gender) {
+        JSONObject userJson = new JSONObject();
+        try {
+            userJson.put("userId", userID);
+            userJson.put("age", age);
+            userJson.put("sex", gender);
+            userJson.put("date", getCurrentTimestamp());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return userJson;
+    }
 
-
+    private String getCurrentTimestamp() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String stringTime = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(timestamp);
+        Log.i("Timestamp",stringTime );
+        return stringTime;
+    }
 
 
 }
--- app/src/test/java/com/hey/idbyimage/idbyimage/Utils/DataCollectorTest.java
+++ app/src/test/java/com/hey/idbyimage/idbyimage/Utils/DataCollectorTest.java
@@ -1,8 +1,12 @@
 package com.hey.idbyimage.idbyimage.Utils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +14,7 @@ public class DataCollectorTest {
     DataCollector dc = DataCollector.getDataCollectorInstance();
     @Before
     public void setUp() throws Exception {
+
     }
 
     @After
@@ -26,7 +31,47 @@ public class DataCollectorTest {
         }
     }
 
+    @Test
+    public void testSendUserRatingDataToServer(){
+
+        int randUser = (int)(Math.random()*10000000);
+        int randRating = (int)(Math.random()*10)+1;
+        int randImage = (int)(Math.random()*50)+1;
+        JSONObject userImageRating = new JSONObject();
+
+        try {
+            userImageRating.put("userId", "TEST_USER_"+randUser);
+            userImageRating.put("imageId", "p"+randImage);
+            userImageRating.put("rating", randRating+"");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            fail();
+        }
+        DataCollector dc = DataCollector.getDataCollectorInstance();
+        boolean ans = dc.sendDataToServer(userImageRating, "/ratings");
+        assertTrue(ans);
+    }
 
 
+    @Test
+    public void testSendAllUserRatingsToServer() {
+        HashMap<String, Integer> allImages = new HashMap<>();
+        for (int i=1; i<51; i++){
+            allImages.put("p"+i,(int)(Math.random()*10)+1);
+        }
+        DataCollector dc = DataCollector.getDataCollectorInstance();
+        dc.setImageRatingsData(allImages);
+        int randUser = (int)(Math.random()*10000000);
+        boolean ans = dc.sendAllUserRatingsToServer("TestAllRatingsUser_"+randUser);
+        assertTrue(ans);
+    }
 
+    @Test
+    public void testSendUserDataToServer() {
+        String randUser = "UserDataTest_" + (int)(Math.random()*10000000);
+        String randAge = (int)(Math.random()*100)+15 + "";
+        DataCollector dc = DataCollector.getDataCollectorInstance();
+        boolean ans = dc.sendUserDataToServer(randUser, randAge, "TEST_GENDER");
+        assertTrue(ans);
+    }
 }
